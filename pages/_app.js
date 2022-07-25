3deaa061ecdba5556731f10bc3b9334bd17e71de
Hristo Petrov <king0fblader98@mail.bg>
2022-07-26 02:44:14 +0300

    create navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+	<ThemeProvider attribute="class">
+		<div className="dark:bg-nft-dark bg-white min-h-screen">
+			<Navbar />
+			<Component {...pageProps} />
+			<Footer />
+		</div>
+
+		<Script
+			src="https://kit.fontawesome.com/684c3ce875.js"
+			crossorigin="anonymous"
+		></Script>
+	</ThemeProvider>
+);
+
+export default MyApp;
