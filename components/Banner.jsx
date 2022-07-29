f10fecd46f2de56a84251dd9d50757254800d929
Hristo Petrov <king0fblader98@mail.bg>
2022-07-29 14:15:28 +0300

    create Top Sellers section

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -10,8 +10,8 @@ const Banner = ({ parentStyles, childStyles, name }) => {
 			>
 				{name}
 			</p>
-			<div className="absolute w-48 h-48 sm:w-32 sm:h-32 rounded-full white-bg -top-9 -left-16 -z-5" />
-			<div className="absolute w-72 h-72 sm:w-56 sm:h-56 rounded-full white-bg -bottom-24 -right-14 -z-5" />
+			<div className="absolute w-48 h-48 sm:w-32 sm:h-32 rounded-full white-bg -top-9 -left-16 -z-5 drop-shadow-[5px_5px_3px_rgba(0,0,0,0.25)]" />
+			<div className="absolute w-72 h-72 sm:w-56 sm:h-56 rounded-full white-bg -bottom-24 -right-14 -z-5 drop-shadow-[-5px_0_3px_rgba(0,0,0,0.25)]" />
 		</div>
 	);
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,49 @@
-import { Banner } from '../components';
+import { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+
+import { Banner, CreatorCard } from '../components';
+import images from '../assets';
+import { makeId } from '../utils/makeId';
 
 const Home = () => {
+	const [hideButtons, setHideButtons] = useState(false);
+	const parentRef = useRef(null);
+	const scrollRef = useRef(null);
+	const { theme } = useTheme();
+
+	const handleScroll = (direction) => {
+		const { current } = scrollRef;
+
+		const scrollAmount = window.innerWidth > 1800 ? 250 : 220;
+
+		if (direction === 'left') {
+			current.scrollLeft -= scrollAmount;
+		} else {
+			current.scrollLeft += scrollAmount;
+		}
+	};
+
+	const isScrollable = () => {
+		const { current } = scrollRef;
+		const { current: parent } = parentRef;
+
+		if (current?.scrollWidth >= parent?.offsetWidth) {
+			setHideButtons(false);
+		} else {
+			setHideButtons(true);
+		}
+	};
+
+	useEffect(() => {
+		isScrollable();
+		window.addEventListener('resize', isScrollable);
+
+		return () => {
+			window.removeEventListener('resize', isScrollable);
+		};
+	});
+
 	return (
 		<div className="flex justify-center sm:px-4 p-12">
 			<div className="w-full minmd:w-4/5">
@@ -9,6 +52,58 @@ const Home = () => {
 					childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left text-white"
 					parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
 				/>
+				<div>
+					<h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sx:ml-0">
+						Top Sellers
+					</h1>
+					<div
+						className="relative flex-1 max-w-full flex mt-3 mx-5"
+						ref={parentRef}
+					>
+						<div
+							className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+							ref={scrollRef}
+						>
+							{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+								<CreatorCard
+									key={`creator-${i}`}
+									rank={i}
+									creatorImage={images[`creator${i}`]}
+									creatorName={`0x${makeId(3)}...${makeId(4)}`}
+									creatorEths={10 - i * 0.5}
+								/>
+							))}
+							{!hideButtons && (
+								<>
+									<div
+										onClick={() => handleScroll('left')}
+										className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer -left-5"
+									>
+										<Image
+											src={images.left}
+											layout="fill"
+											objectFit="contain"
+											alt="left_arrow"
+											className={theme === 'light' && 'filter invert'}
+										/>
+									</div>
+									<div
+										onClick={() => handleScroll('right')}
+										className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer -right-5"
+									>
+										<Image
+											src={images.right}
+											layout="fill"
+											objectFit="contain"
+											alt="right_arrow"
+											className={theme === 'light' && 'filter invert'}
+										/>
+									</div>
+								</>
+							)}
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
